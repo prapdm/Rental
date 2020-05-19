2f4136a360b2b59ce9355968fe2c309c92e263c7
Paweł <prapdm@users.noreply.github.com>
2020-05-19 11:38:27 +0200

    Clean up code - removed variables which is not used

--- SetupWindow.py
+++ SetupWindow.py
@@ -29,7 +29,7 @@ class SetupWindow():
         tk.Label(master, text="Data base name").grid(row=3, sticky=W, padx=10, pady=5)
         tk.Entry(master, bd=1).grid(row=3, column=1, padx=20, pady=5)
 
-        connection_info = tk.Label(master).grid(row=4, sticky=W, padx=20, pady=5)
+        tk.Label(master).grid(row=4, sticky=W, padx=20, pady=5)
 
         tk.Button(master, text='Test connetion', height=2, width=15).grid(row=5, column=0, padx=10, pady=5)
         tk.Button(master, text='Save', height=2, width=15).grid(row=5, column=1, padx=10, pady=5)
