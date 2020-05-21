4d102c0a7467949a7acb7eed5af79085464a1adb
Paweł <prapdm@users.noreply.github.com>
2020-05-21 16:43:43 +0200

    Clean up

--- SetupWindow.py
+++ SetupWindow.py
@@ -17,18 +17,19 @@ class SetupWindow():
         master.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
         master.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
 
-        tk.Label(master, text="Database adress").grid(row=0, sticky=W, padx=10, pady=5)
+        tk.Label(master, text="Adress").grid(row=0, sticky=W, padx=10, pady=5)
         tk.Entry(master, bd=1).grid(row=0, column=1, padx=20, pady=5)
 
 
         tk.Label(master, text="Port").grid(row=1, sticky=W, padx=10, pady=5)
-        v = StringVar(master, value='3306')
-        tk.Entry(master, bd=1, textvariable=v).grid(row=1, column=1, padx=20, pady=5)
+        tk.Entry(master, bd=1).grid(row=1, column=1, padx=20, pady=5)
 
-        tk.Label(master, text="Database user").grid(row=2, sticky=W, padx=10, pady=5)
+        tk.Label(master, text="Data base user").grid(row=2, sticky=W, padx=10, pady=5)
         tk.Entry(master, bd=1).grid(row=2, column=1, padx=20, pady=5)
 
-        tk.Label(master, text="Database name").grid(row=3, sticky=W, padx=10, pady=5)
+        # pole haslo
+
+        tk.Label(master, text="Data base name").grid(row=3, sticky=W, padx=10, pady=5)
         tk.Entry(master, bd=1).grid(row=3, column=1, padx=20, pady=5)
 
         tk.Label(master).grid(row=4, sticky=W, padx=20, pady=5)
@@ -40,3 +41,75 @@ class SetupWindow():
 
     def __del__(self):
         return
+
+
+    # Test connection method
+    def test_connection(self, user, password, host, database):
+
+        try:
+            cnx = mysql.connector.conect(user = user, password = password, host = host, database = database)
+        except mysql.connector.Error as err:
+
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+
+            cnx.close()
+
+
+    # Save settings to database.cfg file
+    def save_settings(self, user, password, host, database):
+        # sciezka do naszego pliku
+        my_fille = Path("database.cfg")
+        # otwieramy plik do edycji - plik powinien byc za kazdym razem czyszczony (do sprawdzenia czy tak to dziala)
+        config = open(my_file, 'w')
+        # zapisujemy dane
+        config.write(user)
+        config.write(password)
+        config.write(host)
+        config.write(database)
+        # zamykamy plik
+        config.close()
+        # zwracamy wartosc
+        return "Your connection data has been saved to a file database.cfg"
+
+
+
+    # Test connection method
+    def test_connection(self, user, password, host, database):
+
+        try:
+            cnx = mysql.connector.conect(user = user, password = password, host = host, database = database)
+        except mysql.connector.Error as err:
+
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+
+            cnx.close()
+
+
+    # Save settings to database.cfg file
+    def save_settings(self, user, password, host, database):
+        # sciezka do naszego pliku
+        my_fille = Path("database.cfg")
+        # otwieramy plik do edycji - plik powinien byc za kazdym razem czyszczony (do sprawdzenia czy tak to dziala)
+        config = open(my_file, 'w')
+        # zapisujemy dane
+        config.write(user)
+        config.write(password)
+        config.write(host)
+        config.write(database)
+        # zamykamy plik
+        config.close()
+        # zwracamy wartosc
+        return "Your connection data has been saved to a file database.cfg"
+
