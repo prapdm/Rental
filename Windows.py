6f7872b8ed14f6e439978a5d043c8569a88669a2
Paweł <prapdm@users.noreply.github.com>
2020-05-21 16:48:23 +0200

    Read database.cfg file

--- Windows.py
+++ Windows.py
@@ -1,17 +1,50 @@
 from MainWindow import MainWindow
 from SetupWindow import SetupWindow
 from pathlib import Path
-
+import mysql.connector
+from mysql.connector import errorcode
 
 class Windows:
     # konstruktor
     def __init__(self):
         # plik gdzie będziemy przechowywać dane do bazy sql
-        my_file = Path("database.cfg")
+        filename = "database.cfg"
+        my_file = Path(filename)
         if my_file.is_file():
-            self.show_main_window()
-        else:
-            self.show_setup_window()
+            # pusta lista
+            lines = []
+            # otwieramy plik w trybie do odczytu
+            config = open(filename, "r")
+            # pętla przez każdą linię
+            for line in config:
+                lines.append(line)
+
+            # jestli plik nie jest pusty (minimum 5 lini) sprawdź połączenie
+            if len(lines) > 4:
+
+                user = lines[0].rstrip()
+                password = lines[1].rstrip()
+                host = lines[2].rstrip()
+                database = lines[3].rstrip()
+                port = lines[4].rstrip()
+                # zanim odpalimy główne okno sprawdzmy czy jest połączenie z bazą.
+                try:
+                    cnx = mysql.connector.connect(user=user, password=password, host=host, database=database, port=port)
+
+                    if cnx.is_connected:
+                        # jest połaczenie odpal główne okno
+                        self.show_main_window()
+                    else:
+                        # nie ma połączenia - okno setup
+                        self.show_setup_window()
+                except mysql.connector.Error as err:
+                    print(err.errno)
+                    self.show_setup_window()
+
+            else:
+                 # plik nie ma wszystkich danych
+                 self.show_setup_window()
+
 
     # glówne okno programu
     def show_main_window(self):
