e9d7b67d4ac769f7e6edb453133d57ee48e9a400
Paweł <prapdm@users.noreply.github.com>
2020-05-29 14:47:22 +0200

    Search user query

--- FindUserWindow.py
+++ FindUserWindow.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import tkinter as tk
 from tkinter import *
+from ResultWindow import ResultWindow
+from tkinter import messagebox
 
 class FindUserWindow:
     def __init__(self, cnx):
@@ -44,15 +46,46 @@ class FindUserWindow:
         tk.Button(master, text='Cancel', height=2, width=15, command=self.close_windows).grid(row=6, column=0, padx=10, pady=5)
 
         # przycisk szukaj
-        tk.Button(master, text='Search', height=2, width=15, command=self.close_windows).grid(row=6, column=1, padx=10, pady=5)
+        tk.Button(master, text='Search', height=2, width=15, command=lambda: self.search_user(name.get(), surname.get(), adress.get(), phone.get(), email.get(), ean.get())).grid(row=6, column=1, padx=10, pady=5)
 
-    # def search_user(self, name, surname, adress, phone, email,ean):
+    def search_user(self, name, surname, adress, phone, email, ean):
 
         if self.cnx.is_connected() != True:
             # nie ma polaczenia wiec zrob reconnect
             self.cnx.reconnect()
 
-        zapytanie_sql = "SELECT * FROM users "
+
+
+        # poczatek zapytania SQL
+        zapytanie = "SELECT * FROM users "
+
+        if len(name) == 0:
+            zapytanie += "WHERE name LIKE \"%%\" "
+        else:
+            zapytanie += "WHERE name LIKE \"%"+name+"%\" "
+
+        if len(surname) > 0:
+            zapytanie += "AND surname LIKE \"%"+surname+"%\" "
+        if len(adress) > 0:
+            zapytanie += "AND adress LIKE \"%"+adress+"%\" "
+        if len(phone) > 0:
+            zapytanie += "AND phone LIKE \"%"+phone+"%\" "
+        if len(email) > 0:
+            zapytanie += "AND email LIKE \"%"+email+"%\" "
+        if len(ean) > 0:
+            zapytanie+= "AND ean ="+ean+" "
+
+        zapytanie+= "ORDER BY name ASC"
+        print(zapytanie)
+
+        # odpytaj baze
+        cursor = self.cnx.cursor()
+        cursor.execute(zapytanie)
+        records = cursor.fetchall()
+
+        # otworzmy nowe okno z wynikami
+        ResultWindow(self.cnx, records)
+        self.master.destroy()
 
 
 
