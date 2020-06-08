f4ccd4e5c6856a9d139345536c1969c37f23e185
Paweł <prapdm@users.noreply.github.com>
2020-06-08 13:40:51 +0200

    Added filter1 in ResultUserWindow

--- ResultUserWindow.py
+++ ResultUserWindow.py
@@ -17,15 +17,19 @@ class ResultUserWindow:
         self.master = master
         self.cnx = cnx
 
-        Checkbutton(master, text="users who have borrowed minimum", variable=1).grid(row=0,  column=0, sticky=W)
-        Checkbutton(master, text="days after the deadline:", variable=1).grid(row=1, column=0,   sticky=W)
-
         v1 = StringVar(master, value='1')
-        q1 = tk.Entry(master, bd=1, textvariable=v1)
+        v1.trace("w", lambda name, index, mode, v1=v1: self.filter_1(q1.get()))
+        q1 = tk.Entry(master, bd=1, textvariable=v1,state="disabled")
         q1.grid(row=0, column=0, padx=250, pady=5, sticky=W)
+        self.q1 = q1
+
         v2 = StringVar(master, value='1')
         q2 = tk.Entry(master, bd=1, textvariable=v2)
         q2.grid(row=1, column=0, padx=250, pady=5, sticky=W)
+
+        checkbox1 = Checkbutton(master, text="users who have borrowed minimum", variable=1, command=lambda: self.borrowed_days_click( q1.get()))
+        checkbox1.grid(row=0,  column=0, sticky=W)
+        checkbox2 = Checkbutton(master, text="days after the deadline:", variable=2).grid(row=1, column=0,   sticky=W)
 
         self.show_table(records)
 
@@ -66,8 +70,36 @@ class ResultUserWindow:
         scroll.configure(command=table.yview)
         table.configure(yscrollcommand=scroll.set)
 
-
         return table
 
-    def destroy_table(self,table):
-        table.delete()
+    def borrowed_days_click(self, days):
+
+        if self.q1["state"] == "disabled":
+            self.q1.config(state='normal')
+        else:
+            self.q1.config(state='disabled')
+
+        self.filter_1(days)
+
+
+    def filter_1(self, days):
+
+        if self.cnx.is_connected() != True:
+            # nie ma polaczenia wiec zrob reconnect
+            self.cnx.reconnect()
+
+        if self.q1["state"] == "normal":
+            zapytanie = "SELECT * FROM users LEFT JOIN rentals ON users.id = rentals.user_id WHERE rentals.to_date < CURDATE() -" + days + " ORDER BY name ASC"
+        else:
+            zapytanie = "SELECT * FROM users ORDER BY name ASC"
+
+        print(zapytanie)
+
+        # odpytaj baze
+        cursor = self.cnx.cursor()
+        cursor.execute(zapytanie)
+        records = cursor.fetchall()
+
+        self.show_table(records)
+
+
